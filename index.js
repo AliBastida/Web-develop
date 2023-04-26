5b903f5bc8c6b933bec77e1bc172a0828ee0825c
Alicia  Bastida <ali@Mac-mini-de-Alicia.local>
2023-04-26 11:10:18 +0200

    modificados

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const about_me = document.getElementById('about_me');
 about_me.addEventListener('mouseover', showtext);
 
 function showtext(){
-    const about_me = document.createElement('about_me');
-    document.body.appendChild(h2);
+    const ul = document.createElement('ul');
+    ul.innerHTML = 
+    '<li>Breathing since 1984</li> <li>Married in 2010</li> <li>Became a Mom in 2013</li> <li>Became a Mom again in 2015</li><li>Became a student of Software developer in 2022</li>';
+    document.body.appendChild(ul);
 }
