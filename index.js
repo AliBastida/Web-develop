2195c955279da4cc5a762386a8fbdc6cec0c8145
Alicia  Bastida <ali@Mac-mini-de-Alicia.local>
2023-04-26 10:41:06 +0200

    subo .js

---
+++ index.js
@@ -0,0 +1,7 @@
+const about_me = document.getElementById('about_me');
+about_me.addEventListener('mouseover', showtext);
+
+function showtext(){
+    const about_me = document.createElement('about_me');
+    document.body.appendChild(h2);
+}
